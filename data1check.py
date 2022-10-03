681fdc4eda82fffe18fe83bd116cb72bc56070e4
Justin <112827610+Jus1nM@users.noreply.github.com>
2022-10-02 23:17:14 -0400

    Add files via upload

---
+++ data1check.py
@@ -0,0 +1,19 @@
+#Code Louisville: Data 1 knowledge check
+#Print statement that says "hellow world!"
+print("Hello World!")
+#another way
+greeting = "Hello World!"
+print(greeting)
+#Make a list populated with several values. Then, print() one of those values to the prompt.
+list = ['Miles Davis', 'Thelonious Monk', '1', '2', '5.5']
+print(list[0])
+print(type(list))
+#Make a dictionary populated with two keys and two values. Print one of the values.
+music = {'Miles Davis':'Kind of Blue', 'Thelonious Monk': 'Blue Monk'}
+print(music['Thelonious Monk'])
+print(type(music))
+#Make a tuple with 4 values. Print one of them.
+tuple = (1, 2, 3, 4)
+print(tuple[1])
+print(type(tuple))
+
